120ebfe7780cfc695a32e1ae178899e280bc6c9a
willynganga <willy.mwangi@acrolinx.com>
2024-01-09 10:35:23 +0300

    refactor: use table driven tests for Area tests

--- structs-methods-interfaces/structs_methods_interfaces_test.go
+++ structs-methods-interfaces/structs_methods_interfaces_test.go
@@ -13,20 +13,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{10.0, 10.0}, want: 100},
+		{shape: Circle{10.0}, want: 314.1592653589793},
+		{shape: Triangle{12, 6}, want: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+		}
+	}
 }
